76220ec5cf304bb347587fcbc4d26b2ae3db546f
Vasiliy Petunin <vasyusha_petunin@mail.ru>
2022-09-20 22:29:07 +0300

    lesson1HW

---
+++ lesson1/src/Message.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Message.css'
+
+function Message(props) {
+  return (
+    <h1>
+        Message: {props.greeting}
+    </h1>
+  );
+}
+
+export default Message;
